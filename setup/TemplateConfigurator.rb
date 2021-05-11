8cef3ccee471b6ccef585b64edb6f444584a6a22
qinxiaokang <qinxiaokang@ppdai.com>
2021-05-11 10:44:04 +0800

    脚本修改

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -89,7 +89,7 @@ module Pod
       # replace_variables_in_files
       clean_template_files
       # rename_template_files
-      add_pods_to_podfile
+      # add_pods_to_podfile
       # customise_prefix
       rename_classes_folder
       # ensure_carthage_compatibility
